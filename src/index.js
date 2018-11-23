d15ea9b6a06c1d7f066f59cbc64100a52b233384
Henry Do <661034+vitaminh@users.noreply.github.com>
2018-11-23 15:46:06 -0800

    startcomplete

--- src/index.js
+++ src/index.js
@@ -2,21 +2,43 @@ const { GraphQLServer } = require("graphql-yoga");
 
 // 1
 const typeDefs = `
-type Query {
-    info: String!
-}
+    type Query {
+        info: String!
+        feed: [Link!]!
+    }
+
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
+    }
+
 `;
+
+// dummy data
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
 
 // 2
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`
-    }
-}
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+  },
+  Link: {
+      id: (root) => root.id,
+      description: (root) => root.description,
+      url: (root) => root.url
+  }
+};
 
 // 3
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers,
-})
+  typeDefs,
+  resolvers
+});
 server.start(() => console.log(`Server is running on http://localhost:4000`));
