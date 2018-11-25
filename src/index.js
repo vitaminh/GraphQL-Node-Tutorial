d1491ccde636a69a4935173905ac31030f017a38
Henry Do <661034+vitaminh@users.noreply.github.com>
2018-11-24 17:14:42 -0800

    Prisma Deployed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-// 1
-const typeDefs = `
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
-
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-
-`;
-
 // dummy data
 let links = [
   {
@@ -24,21 +9,29 @@ let links = [
   }
 ];
 
-// 2
+let idCount = links.length;
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
   },
-  Link: {
-      id: (root) => root.id,
-      description: (root) => root.description,
-      url: (root) => root.url
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link);
+      return link;
+    }
   }
 };
 
-// 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 });
+
 server.start(() => console.log(`Server is running on http://localhost:4000`));
